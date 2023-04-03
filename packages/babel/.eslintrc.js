7563b78140dbb713fc58436ddf7f75f82872b3c6
jihye1 <36979920+jihye1@users.noreply.github.com>
2023-04-03 11:57:30 +0900

    Delete packages/babel directory

--- packages/babel/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//   parser: "@babel/eslint-parser",
-//   //parser: "babel-eslint",
-//   extends: ["plugin:prettier/recommended", "airbnb"],
-//   parserOptions: {
-//     requireConfigFile: false
-//   }
-// };
