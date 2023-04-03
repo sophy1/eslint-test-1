ef9a463564a124b346ce833775f9a0304e7cc313
jihye1 <36979920+jihye1@users.noreply.github.com>
2023-04-03 11:39:11 +0900

    Create .eslintrc.js

---
+++ packages/babel/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  //parser: "babel-eslint",
+  extends: ["plugin:prettier/recommended", "airbnb"],
+  parserOptions: {
+    requireConfigFile: false
+  }
+};
