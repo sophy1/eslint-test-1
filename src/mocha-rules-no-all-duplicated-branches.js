b466ddf6db2b444a826ba0f73949fc16cfd644a9
jihye1 <36979920+jihye1@users.noreply.github.com>
2023-07-10 12:51:08 +0900

    Update mocha-rules-no-all-duplicated-branches.js

--- src/mocha-rules-no-all-duplicated-branches.js
+++ src/mocha-rules-no-all-duplicated-branches.js
@@ -1,7 +1,5 @@
 it('should work', function (done) {
     fetchData(options, function (error, data) {
         expect(error).not.to.be.ok;
-        expect(data).to.deep.equal({ foo: 'bar' });
-        // done callback was not called
     });
 });
